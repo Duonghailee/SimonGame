2d07544136b5f1d2638267d81f856cf12aa26a8e
Duonghailee <duonghailee@gmail.com>
2017-09-16 22:50:37 +0300

    fixed interval time problem, now only strict mode works

--- script.js
+++ script.js
@@ -4,46 +4,66 @@
 var display = document.getElementById("digital-display");
 var count = document.getElementById("digital-display").getAttribute("value");
 var correct = true; // use to track user correct choice
+var gameStatus = "off"; // use to track game is on/off
+var strictMode = document.getElementById("strictMode"); //this variable tracks the strict mode on/off
+
 const whichPart = ['green', 'red', 'yellow', 'blue'];
 const tones = {
     green: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'),
     red: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'),
     yellow: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'),
-    blue: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
+    blue: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'),
+    playerTurn: new Audio('http://soundjax.com/reddo/40725%5EDING1.mp3'),
+    fail: new Audio('http://soundjax.com/reddo/76527%5Eerror.mp3')
 };
 var generating = 0; // use to track reset the game when user click off
 var player = [];
 var playerIsRight = true; //use to track the game status, if player is on winning 
 
+var nodes = document.getElementsByClassName("quarter");
+var els = [].slice.call(nodes);
 
+/* this function turn on / off the game */
 function toggle() {
     var slider = document.getElementById("slider");
     var status = slider.getAttribute("value"); // on or off
     var turn = status === "off" ? "on" : "off";
-    var turnOn = status === "off" ? true : false;
-
-    slider.style.cssFloat = turnOn === true ? "right" : "left"; //slide the toggle
+    gameStatus = turn; // this varible pass the game status: on/off to the start button
+    slider.style.cssFloat = turn === "on" ? "right" : "left"; //slide the toggle
     slider.setAttribute("value", turn);
+    //console.log("status now is: " + gameStatus);
+    if (turn === "off") {
+        clearGame();
+    }
 
-    var reset = turnOn === true ? startGame() : clearGame();
+}
+
+/* this funct enable/disable strict mode */
+function setStrict() {
+
+    strictMode.setAttribute("value", strictMode.getAttribute("value") === "off" ? "on" : "off");
+    strictMode.classList.toggle("lighten");
+    // console.log("strictmode is: " + strictMode.getAttribute("value"));
 }
 
 function startGame() {
-    display.innerText = "- -";
-    document.getElementById("start-btn").onclick = function() {
-        document.getElementById("start-btn").style.pointerEvents = "none"; // to block start from further clicking
-        var simon = new game();
-        simon.start();
-        count = 0;
-    }
+    if (gameStatus === "on") {
+        //this funct helps blinking the - - in the display panel
+        clearGame(); //clear the last played game first;
+        count = "- -";
+        display.innerHTML = "<span class='blink'>" + count + "</span";
 
+        //start new game here
+        var simon = new game();
+        count = 0;
+        simon.start();
+
+    } else {
+        alert("please turn on the game");
+    }
 }
 
 function clearGame() {
-    document.getElementById("start-btn").style.pointerEvents = "auto"; // to unblock start button
-    document.getElementById("start-btn").onclick = function() {
-        alert("please turn on game");
-    }
     count = "";
     updateCount();
     clearInterval(generating);
@@ -59,87 +79,114 @@ function game() {
     this.isOn = false;
 
     this.reset = false;
-    this.strictMode = false;
-    this.getStrict = function() {
-        return this.strictMode;
-    };
-    this.getStatus = function() {
-        return this.isOn;
-    };
-    this.restart = function() {
-        this.reset = true;
-    }
-    this.setStrictMode = function() {
-        this.strictMode = true;
-    }
-    this.getCount = function() {
-        return this.count;
-    }
+
     var simonPattern = [];
     this.player = [];
 
     this.start = function() {
-
+        var move = { step: 0, colors: [] };
         if (playerIsRight) {
-            generateMove();
-        }
 
-
-        function generateMove() {
             var t_pattern = 2000;
+
             generating = setInterval(function() {
-                var move = { step: 0, colors: [] };
-                for (var i = 0; i <= count; i++) {
-                    var color = whichPart[(Math.floor(Math.random() * 4))];
-                    move.colors.push(color);
-                }
+
+                var color = whichPart[(Math.floor(Math.random() * 4))];
+                move.colors.push(color);
+
                 move.step = count;
                 simonPattern.push(move);
                 console.log(move);
                 count++;
-                if (count === 1) {
-                    clearInterval(generating);
-                }
-
                 lighten(move.colors);
-                updateCount();
-                checkWin();
-                console.log("count is update: " + count);
-                /*setTimeout(function() {
-                    darkenColor(move.colors);
-                }, 400);*/
+                console.log(move.colors);
+
+
+
             }, t_pattern += 2000);
         }
-
     }
+
+
 }
 
 function lighten(colors) {
     var i = 0;
     var queue = setInterval(function() {
         lightenColor(colors[i]);
-        i++;
-        if (i >= colors.length) {
-            clearInterval(queue);
-        }
-    }, 800);
+        setTimeout(function() {
+            i++;
+            if (i >= colors.length) {
+                clearInterval(queue); //lighten and darken a set of colors has finished
+                //now is player turn, 
+                updateCount();
+                tones.playerTurn.play(); //notify player's turn
+                console.log("player's turn");
+                checkWin(colors);
+
+            }
+        }, 300); //300ms because it let the lightColor finished first, which takes about 100ms
+
+    }, 1000);
 }
 
-function lightenColor(element) {
-    var el = document.getElementById(element);
+function lightenColor(color) {
+    var el = document.getElementById(color);
     el.classList.add("lighten");
-    tones[element].play();
-    console.log("color: " + element + " brighten");
+    tones[color].play();
+    console.log("color: " + color + " brighten");
     setTimeout(function() {
         el.classList.remove("lighten");
-        console.log("color: " + element + " darken");
-    }, 300);
+        console.log("color: " + color + " darken");
+    }, 200);
 }
 
-function checkWin() {
-    document.getElementsByClassName("quarter").onclick = function() {
-        console.log(this.id);
+function checkWin(colors) {
+    console.log("test check win function");
+    if (!match(colors)) {
+        if (strictMode.getAttribute("value") === "on") {
+            console.log("you lose");
+            clearGame();
+            tones.fail.play();
+        }
+        /*else {
+                   setTimeout(function() {
+
+                   }, 1000); // not in strict mode
+                   if (match(colors)) {
+                       console.log("congrat, you can continue");
+                   } else {
+
+                   }
+                   console.log("please try again");
+               } */
+    } else {
+        console.log("player click the right pattern");
     }
+}
+
+function match(colors) {
+    player = []; //reset each round;
+    els.forEach(function(element) {
+        element.onclick = function() {
+            // console.log(element.id + " color is clicked");
+            player.push(element.id);
+            tones[element.id].play();
+            console.log("player input :" + player);
+        }
+
+    }, this);
 
 
+    //compare pattern
+    if (colors.length == player.length && player.every(function(e, i) {
+            return e === colors[i]
+        })) {
+        console.log("colors length: " + colors.length + "and player input length: " + player.length);
+        return true;
+        console.log("right");
+    } else {
+        console.log("compare false");
+        return false;
+    }
 }
